c4318e172c9ae57d2baff7fcca615fc2e7688911
Anmol Ghadia <aghadia@aloe.cs.ubc.ca>
2026-07-24 14:54:38 -0700

    captured missing dependencies for libtiff

--- database/libtiff/fetch.sh
+++ database/libtiff/fetch.sh
@@ -25,3 +25,7 @@ mv code latest
 cp ./build.sh ./lib.toml latest
 
 echo "Repository successfully cloned and copied to '$TARGET_DIR' and latest."
+
+echo "fetching dependencies"
+# may need to modif if running on host with without apt
+apt install libjpeg-dev libjbig-dev liblzma-dev libzstd-dev
